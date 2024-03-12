36beb7b998c965f514feae4378cc748a7b55426b
Hernán Cussi <hcussi@gmail.com>
2024-03-12 17:04:31 -0300

    Added logging microservice base structure

---
+++ logging-service/src/test/java/org/hernan/cussi/lyrics/loggingservice/LyricsLoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hernan.cussi.lyrics.loggingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LyricsLoggingServiceApplication.class)
+class LyricsLoggingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
