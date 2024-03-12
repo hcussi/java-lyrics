d899a5307eecaebf25fa7581699ab1cb67de940a
Hernán Cussi <hcussi@gmail.com>
2024-03-12 17:05:06 -0300

    Added monitor microservice base structure

---
+++ monitor-service/src/test/java/org/hernan/cussi/lyrics/monitorservice/LyricsMonitorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hernan.cussi.lyrics.monitorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LyricsMonitorServiceApplication.class)
+class LyricsMonitorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
